29c363b06bc962920dcdfe5f52241d7042798d78
Eugene Che <eugeneyche@gmail.com>
2013-09-30 15:10:10 -0700

    Did PR1 to completion, moved programs to program1 inside to allow more programs to be easilly stored later on.

--- programs/program1/Fred.java
+++ programs/program1/Fred.java
@@ -7,10 +7,10 @@
 import objectdraw.*;
 import java.awt.*;
 
-public class Fred extends FrameWindowController 
+public class Fred extends WindowController 
 {
     public void onMousePress(Location point) {
-        new FilledOval(point.getX(), point.getY(), 30, 30, canvas).setColor(new Color(0,0,128));
+        new FilledOval(point.getX(), point.getY(), 30, 30, canvas).setColor(Color.BLUE);
         new Line(point.getX(), point.getY(), point.getX() + 45, point.getY(), canvas);
         new Line(point.getX(), point.getY(), point.getX(), point.getY() + 45, canvas);
     }
